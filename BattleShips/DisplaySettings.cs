2317bc90fc9b8293caabf9fc771e859436521f44
Michał Kutzner <kucyk010@gmail.com>
2019-08-30 21:58:43 +0200

    Displaying board and Program.cs refactoring

---
+++ BattleShips/DisplaySettings.cs
@@ -0,0 +1,9 @@
+﻿namespace BattleShips
+{
+    static class DisplaySettings
+    {
+        public const char HIT_SYMBOL = 'X';
+        public const char MISS_SYMBOL = 'O';
+        public const char EMPTY_SYMBOL = '*';
+    }
+}
--- BattleShips/Program.cs
+++ BattleShips/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Battleships.Core;
 using Battleships.Core.Enums;
+using Battleships.Core.Interfaces;
 using Battleships.Core.Models;
 using Battleships.Core.Utils;
 using System;
@@ -10,38 +11,15 @@ namespace BattleShips
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Battleship game");
+            Console.WriteLine("BATTLESHIP GAME\n");
+
             var game = CreateGame();
             game.Start();
-            Console.WriteLine("Ships placed. You can start playing.");
 
-            while (game.Active)
-            {
-                Console.WriteLine("Enter coordinates:");
-                var coords = Console.ReadLine();
+            DisplayLegend();
+            Console.WriteLine();
 
-                ShotResult result;
-                try
-                {
-                    result = game.Shoot(coords);
-                }
-                catch (ArgumentException exc) 
-                {
-                    Console.WriteLine($"Error! {exc.Message} Try again.");
-                    continue;
-                }
-
-                if (result.IsHit)
-                {
-                    Console.WriteLine($"Hit! Ship: {result.HitShipType}");
-
-                    if (result.HitShipDestroyed) Console.WriteLine("Sunk!");
-                }
-                else
-                {
-                    Console.WriteLine("Miss");
-                }
-            }
+            MainGameLoop(game);
 
             Console.WriteLine("Game over.");
             Console.ReadKey();
@@ -57,6 +35,85 @@ namespace BattleShips
             var boardInitializer = new BoardInitializer(shipGenerator, randomDataProvider);
 
             return new Game(board, coordinateTranslator, boardInitializer);
+        }
+
+        private static void DisplayLegend()
+        {
+            Console.WriteLine("Board symbols:");
+            Console.WriteLine($"Unknown: {DisplaySettings.EMPTY_SYMBOL}");
+            Console.WriteLine($"Hit: {DisplaySettings.HIT_SYMBOL}");
+            Console.WriteLine($"Missed: {DisplaySettings.MISS_SYMBOL}");
+        }
+
+        private static void MainGameLoop(Game game)
+        {
+            while (game.Active)
+            {
+                DisplayBoard(game);
+                Console.WriteLine();
+
+                Console.WriteLine("Enter coordinates:");
+                var coords = Console.ReadLine();
+
+                ShotResult result;
+                try
+                {
+                    result = game.Shoot(coords);
+                }
+                catch (ArgumentException exc)
+                {
+                    Console.WriteLine($"Error! {exc.Message} Try again.");
+                    continue;
+                }
+
+                if (result.IsHit)
+                {
+                    Console.WriteLine($"Hit! Ship: {result.HitShipType}");
+
+                    if (result.HitShipDestroyed) Console.WriteLine("Sunk!");
+                }
+                else
+                {
+                    Console.WriteLine("Miss");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void DisplayBoard(IGame game)
+        {
+            for (int i = 0; i < Settings.BOARD_SIZE; i++)
+            {
+                for (int j = 0; j < Settings.BOARD_SIZE; j++)
+                {
+                    var currentField = game.BoardFields[i, j];
+                    char symbol = GetDisplaySymbol(currentField);
+
+                    Console.Write($"{symbol} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static char GetDisplaySymbol(IField currentField)
+        {
+            char symbol;
+            switch (currentField.State)
+            {
+                case FieldState.HIT:
+                    symbol = DisplaySettings.HIT_SYMBOL;
+                    break;
+                case FieldState.MISS:
+                    symbol = DisplaySettings.MISS_SYMBOL;
+                    break;
+                default:
+                    symbol = DisplaySettings.EMPTY_SYMBOL;
+                    break;
+            }
+
+            return symbol;
         }
     }
 }
