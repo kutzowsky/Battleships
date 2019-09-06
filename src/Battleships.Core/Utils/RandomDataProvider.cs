8d04f24ed578b39cb637698b2c8c3752dc5c09e3
Michał Kutzner <kucyk010@gmail.com>
2019-09-06 12:09:31 +0200

    Slightly better Random object usage

--- src/Battleships.Core/Utils/RandomDataProvider.cs
+++ src/Battleships.Core/Utils/RandomDataProvider.cs
@@ -7,19 +7,24 @@ namespace Battleships.Core.Utils
 {
     public class RandomDataProvider : IRandomDataProvider
     {
+        private static Random _random;
+
+        public RandomDataProvider()
+        {
+            _random = new Random();
+        }
+
         public ShipOrientation GetRandomOrientation()
         {
-            Random random = new Random();
-            int randomNumber = random.Next(0, 2);
+            int randomNumber = _random.Next(0, 2);
 
             return (ShipOrientation)randomNumber;
         }
 
         public Point GetRandomStartingPoint(ShipOrientation orientation, byte shipLength)
         {
-            Random random = new Random();
-            var coordinate = random.Next(0, Settings.BOARD_SIZE);
-            var lengthConstraintCoordinate = random.Next(0, Settings.BOARD_SIZE - shipLength);
+            var coordinate = _random.Next(0, Settings.BOARD_SIZE);
+            var lengthConstraintCoordinate = _random.Next(0, Settings.BOARD_SIZE - shipLength);
 
             if (orientation == ShipOrientation.HORIZONTAL)
             {
